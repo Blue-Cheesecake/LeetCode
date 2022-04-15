1ee089871b85dafa9f68db73da509ea356fbc6d5
SINUT WATTANRPORN <s_w.piepie@outlook.com.au>
2022-04-15 16:13:36 +0700

    Time: 82 ms (85.80%), Space: 45.8 MB (22.50%) - LeetHub

---
+++ 1-two-sum/1-two-sum.ts
@@ -0,0 +1,10 @@
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+};
