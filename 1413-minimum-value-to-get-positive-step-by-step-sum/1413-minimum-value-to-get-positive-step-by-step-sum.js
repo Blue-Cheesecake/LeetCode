ad3debb6b14605b018e8a92eb786914f7817a082
SINUT WATTANRPORN <s_w.piepie@outlook.com.au>
2022-05-23 15:26:19 +0700

    Time: 91 ms (30.18%), Space: 41.5 MB (96.45%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.js
@@ -6,18 +6,13 @@ var minStartValue = function (nums) {
   const prefixSum = [];
   let sum = 0;
   let isContainNegativeZero = false;
+  let min = 1;
   for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
+    if (sum < min) min = sum;
     if (sum <= 0) isContainNegativeZero = true;
     prefixSum.push(sum);
   }
   if (!isContainNegativeZero) return 1;
-
-  // console.log(prefixSum);
-  // find index of lowest value
-  let index = 0;
-  for (let i = 1; i < prefixSum.length; i++) {
-    if (prefixSum[i] < prefixSum[index]) index = i;
-  }
-  return prefixSum[index] * -1 + 1;
+  return min * -1 + 1;
 };
