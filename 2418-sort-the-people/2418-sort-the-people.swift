d2dd2a6a944731c9adee4338eb7808fdc80b7466
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-10-16 00:50:15 +0700

    Time: 394 ms (45.10%), Space: 15 MB (63.73%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.swift
+++ 2418-sort-the-people/2418-sort-the-people.swift
@@ -4,13 +4,8 @@ class Solution {
 		for i in 0 ..< names.count {
 			map.append((names[i], heights[i]))
 		}
-
-		map.sort { pair1, pair2 in pair1.1 > pair2.1 }
-		let result = map.map { pair in
-			pair.0
-		}
-//		print(map)
-//		print(result)
+		map.sort { $0.1 > $1.1 }
+		let result = map.map { $0.0 }
 		return result
 	}
 }
