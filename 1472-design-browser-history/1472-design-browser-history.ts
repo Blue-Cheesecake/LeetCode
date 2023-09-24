8b4610a2b139a2e01481f18de81d9899695419de
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-09-24 19:59:59 +0700

    Time: 180 ms (26.47%), Space: 56.6 MB (25.73%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.ts
@@ -0,0 +1,52 @@
+class NodeBrowser {
+  value: string;
+  next: NodeBrowser | null;
+  prev: NodeBrowser | null;
+
+  constructor(value: string, next: NodeBrowser | null, prev: NodeBrowser | null) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class BrowserHistory {
+  origin: NodeBrowser;
+  head: NodeBrowser;
+
+  constructor(homepage: string) {
+    this.origin = new NodeBrowser(homepage, null, null);
+    this.head = this.origin;
+  }
+
+  visit(url: string): void {
+    this.head.next = new NodeBrowser(url, null, this.head);
+    this.head = this.head.next;
+  }
+
+  back(steps: number): string {
+    let counter: number = 0;
+    while (counter < steps) {
+      if (this.head.prev !== null) {
+        this.head = this.head.prev;
+        counter++;
+        continue;
+      }
+      break;
+    }
+    return this.head.value;
+  }
+
+  forward(steps: number): string {
+    let i = 0;
+    while (i < steps) {
+      if (this.head.next !== null) {
+        this.head = this.head.next;
+        i++;
+        continue;
+      }
+      break;
+    }
+    return this.head.value;
+  }
+}
