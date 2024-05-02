d4ca6de70aa9a34176c18011d97fe86b70fb8738
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-05-03 02:06:56 +0700

    Time: 2 ms (52.75%), Space: 2.5 MB (20.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,19 +1,28 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let length = nums.len();
-    let mut vec: Vec<i32> = Vec::new();
-    for i in 0..=length - 2 {
-        for j in i + 1..=length - 1 {
-            let x = nums[i];
-            let y = nums[j];
-            if x + y == target {
-                vec.push(i as i32);
-                vec.push(j as i32);
+    let mut result = Vec::new();
+    let mut map: HashMap<i32, usize> = HashMap::new();
+
+    for (i, num) in nums.iter().enumerate() {
+        let find = target - num;
+        let try_finding = map.get(&find);
+
+        match try_finding {
+            Some(j) => {
+                result.push(i as i32);
+                result.push(*j as i32);
+                break;
+            }
+            None => {
+                map.insert(*num, i);
             }
         }
     }
 
-    return vec;
+    return result;
 }
+
 
 }
